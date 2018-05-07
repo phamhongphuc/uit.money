37a69f5d0e21c7c1dd1c295bdfbbc57be60be721
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-05-07 07:20:48 +0700

    Add `adapter`
    Signed-off-by: Phạm Hồng Phúc <phamhongphuc.info@gmail.com>

---
+++ app/src/main/java/uit/money/adapter/FragmentAdapter.java
@@ -0,0 +1,41 @@
+package uit.money.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private int[] layouts = new int[]{};
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+
+    public FragmentAdapter(FragmentManager fragmentManager, int[] layouts) {
+        super(fragmentManager);
+        this.layouts = layouts;
+    }
+
+    public FragmentAdapter(FragmentManager fragmentManager, ArrayList<Fragment> fragments) {
+        super(fragmentManager);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public int getCount() {
+        return Math.max(layouts.length, fragments.size());
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        if (layouts.length != 0) {
+            return new FragmentPage(layouts[i]);
+        } else if (fragments.size() != 0) {
+            return fragments.get(i);
+        } else return null;
+    }
+
+    public Fragment getFragment(int index) {
+        if (fragments == null) return null;
+        else return fragments.get(index);
+    }
+}
---
+++ app/src/main/java/uit/money/adapter/FragmentPage.java
@@ -0,0 +1,24 @@
+package uit.money.adapter;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("ValidFragment")
+public class FragmentPage extends Fragment {
+    protected int mLayout;
+
+    public FragmentPage(@LayoutRes int layout) {
+        mLayout = layout;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(mLayout, container, false);
+    }
+}
