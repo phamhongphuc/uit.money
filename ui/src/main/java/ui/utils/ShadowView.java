486a97ef0884d4a1614d250db63f902ae8ebe612
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-05-29 07:45:47 +0700

    Update Attr, Add ShadowView

---
+++ ui/src/main/java/ui/utils/ShadowView.java
@@ -0,0 +1,11 @@
+package ui.utils;
+
+import android.view.View;
+
+public interface ShadowView {
+    View getView();
+    int getShadowSize();
+    int getDx();
+    int getDy();
+    int getShadowColor();
+}
